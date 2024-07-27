bd8fa20a80b45dafb082c75048f220b44ed2437d
aathikm <aathikm@hotmail.com>
2024-07-27 19:11:08 +0530

    updating the basic version of code

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask_cors import CORS
+import os
+from werkzeug.utils import secure_filename
+from src.pipeline.prediction_pipeline import PredictionPipeline  # Make sure this imports your prediction function
+
+from src.loggingInfo.loggingInfo import logging
+
+app = Flask(__name__)
+# CORS(app)
+CORS(app, resources={f"/*": {"origins": "*"}}) 
+
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        logging.info("the predction function triggered")
+        return redirect(url_for('index'))
+    
+    file = request.files['file']
+    if file.filename == '':
+        logging.info("file is null")
+        return redirect(url_for('index'))
+    
+    if file:
+        logging.info("file existing")
+        filename = secure_filename(file.filename)
+        logging.info(f"fileName: {filename}")
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        logging.info(f"fileName: {file_path}")
+        file.save(file_path)
+        logging.info(f"file saved")
+        
+        # Call your prediction function here
+        predition_obj = PredictionPipeline(file_path)
+        result = predition_obj.predict()
+        logging.info(f"fileName: {result}")
+        
+        return jsonify(result_val=result) #render_template('index.html', prediction=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,8 +1,16 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 # from tensorflow.keras.models import load_model
 # from tensorflow.keras.preprocessing import image
 import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.join(current_dir,"..")
+sys.path.append(src_dir)
+
+from loggingInfo.loggingInfo import logging
 
 class PredictionPipeline:
     def __init__(self,filename):
@@ -10,18 +18,28 @@ class PredictionPipeline:
     
     def predict(self):
         # load model
-        model = tf.keras.models.load_model(os.path.join("artifacts", "training", "model.h5"))
+        model = tf.keras.models.load_model(os.path.join("models", "model.h5"))
+        labels_df_path = os.path.join("models", "labels.csv")
 
         imagename = self.filename
-        print(imagename)
+        logging.info(imagename)
         test_image = tf.keras.preprocessing.image.load_img(imagename, target_size = (224,224))
-        print(test_image)
+        logging.info(test_image)
         test_image = tf.keras.preprocessing.image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+        logging.info(result)
+        labels_df = pd.read_csv(labels_df_path)
+        logging.info(labels_df)
+        labels_df.drop(["Unnamed: 0"], axis=1, inplace=True)
+        logging.info(labels_df)
+        result_val = labels_df["Disease"][labels_df["Code"] == result[0]].values[0]
 
-if __name__ == "__main__":
-    obj = PredictionPipeline(filename= "testImage/image12.JPG")
-    obj.predict()
+        logging.info(result_val)
+        print(result_val)
+        return(result_val)
+
+# if __name__ == "__main__":
+#     obj = PredictionPipeline(filename= "testImage/image12.JPG")
+#     obj.predict()
     
